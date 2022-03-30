eaf60bedbcfd8dca0a626d3f06771c8835f105a1
Alexander Walther <github.com@alexplus.de>
2022-03-30 18:54:51 +0200

    enhance uninstall / update

--- uninstall.php
+++ uninstall.php
@@ -1,9 +1,6 @@
 <?php
 
-# in der uninstall.php sollten Befehle ausgeführt werden, die alle Änderungen, die mit der Installation kamen, entfernen.
-
-# Konfiguration entfernen
-# rex_config::removeNamespace("collect");
+rex_config::removeNamespace("collect");
 
 # YForm-Tabellen löschen (die YForm-Tabellendefinition wird gelöscht, nicht die Datenbank-Tabellen)
 # rex_yform_manager_table_api::removeTable('rex_collect');
