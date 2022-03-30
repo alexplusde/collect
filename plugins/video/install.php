a66edcafd47c5a6045afc115a310fedf808c56a0
Alexander Walther <github.com@alexplus.de>
2022-03-30 19:14:38 +0200

    fix paths part III

--- plugins/video/install.php
+++ plugins/video/install.php
@@ -1,5 +1,5 @@
 <?php
 /* Tablesets aktualisieren */
 
-rex_yform_manager_table_api::importTablesets(rex_file::get($this->getPath('install/rex_collect_video.tableset.json')));
+rex_yform_manager_table_api::importTablesets(rex_file::get(rex_path::plugin("collect", "video", 'install/rex_collect_video.tableset.json')));
 rex_yform_manager_table::deleteCache();
