c8d13ff2af81ec5c5a2f3c340127a2153551f629
Alexander Walther <github.com@alexplus.de>
2022-03-30 20:41:12 +0200

    enhance backend pages

--- pages/collect.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-echo rex_view::title(rex_i18n::msg('test'));
--- pages/index.php
+++ pages/index.php
@@ -2,4 +2,6 @@
 
 if ($subpage = rex_be_controller::getCurrentPagePart(2)) {
     include rex_be_controller::getCurrentPageObject()->getSubPath();
+} else {
+    echo rex_view::title(rex_i18n::msg('collect'));
 }
