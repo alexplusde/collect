0309bd9e4c5c0ca4dcc691b52091e6d369f1e2ea
Alexander Walther <github.com@alexplus.de>
2022-03-30 19:19:04 +0200

    fix typo

--- plugins/places/install.php
+++ plugins/places/install.php
@@ -1,5 +1,5 @@
 <?php
 
 rex_yform_manager_table_api::importTablesets(rex_file::get(rex_path::plugin("collect", "places", 'install/rex_collect_places.tableset.json')));
-rex_yform_manager_table_api::importTablesets(rex_file::get(rex_path::plugin("collect", "places", 'install/rex_collect_places_reviews.tableset.json')));
+rex_yform_manager_table_api::importTablesets(rex_file::get(rex_path::plugin("collect", "places", 'install/rex_collect_places_review.tableset.json')));
 rex_yform_manager_table::deleteCache();
