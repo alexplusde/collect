792c6135e2640f59299f4e8d223ea8153bf236c9
Alexander Walther <github.com@alexplus.de>
2022-03-30 19:16:01 +0200

    fix typo

--- plugins/social_media/boot.php
+++ plugins/social_media/boot.php
@@ -1,6 +1,6 @@
 <?php
 
-if (rex_addon::get('yform')->isAvailable() && !rex::isSafeMode()) ]{
+if (rex_addon::get('yform')->isAvailable() && !rex::isSafeMode()) {
 rex_yform_manager_dataset::setModelClass(
     'rex_collect_social_media',
     collect_social_media::class
