a86c32e404290f03054a4117aa340ebd4efd7a29
Alexander Walther <github.com@alexplus.de>
2022-03-30 19:03:56 +0200

    fix tableset install path for plugins

--- plugins/rss/install.php
+++ plugins/rss/install.php
@@ -1,4 +1,4 @@
 <?php
 
-rex_yform_manager_table_api::importTablesets(rex_file::get(rex_path::addon($this->name, 'install/rex_collect_rss.tableset.json')));
+rex_yform_manager_table_api::importTablesets(rex_file::get(rex_path::plugin($this->name, 'install/rex_collect_rss.tableset.json')));
 rex_yform_manager_table::deleteCache();
--- plugins/video/install.php
+++ plugins/video/install.php
@@ -1,5 +1,5 @@
 <?php
 /* Tablesets aktualisieren */
 
-rex_yform_manager_table_api::importTablesets(rex_file::get(rex_path::addon($this->name, 'install/rex_collect_video.tableset.json')));
+rex_yform_manager_table_api::importTablesets(rex_file::get(rex_path::plugin($this->name, 'install/rex_collect_video.tableset.json')));
 rex_yform_manager_table::deleteCache();
