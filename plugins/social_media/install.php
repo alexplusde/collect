2be756d72561e559e14a67f946953e567293471c
Alexander Walther <github.com@alexplus.de>
2022-03-30 19:07:52 +0200

    fix paths, part II

--- plugins/social_media/install.php
+++ plugins/social_media/install.php
@@ -1,4 +1,4 @@
 <?php
 
-rex_yform_manager_table_api::importTablesets(rex_file::get(rex_path::plugin($this->name, 'install/rex_collect_social_media.tableset.json')));
+rex_yform_manager_table_api::importTablesets(rex_file::get($this->getPath('install/rex_collect_social_media.tableset.json')));
 rex_yform_manager_table::deleteCache();
