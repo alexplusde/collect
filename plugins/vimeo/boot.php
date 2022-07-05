5148668e670d4c8cb3d2a2640609619665a1b3a5
Thomas Skerbis <skerbis@klxm.de>
2022-07-05 17:14:27 +0200

    Vorschaubilder auf allen Seiten

--- plugins/vimeo/boot.php
+++ plugins/vimeo/boot.php
@@ -9,16 +9,13 @@ if (rex_addon::get('cronjob')->isAvailable()) {
     rex_cronjob_manager::registerType(rex_cronjob_collect_video_vimeo::class);
 }
 // Poster image from vimeo.com
-if (rex::isBackend() && rex_request('table_name') == 'rex_collect_vimeo') {  
-    rex_extension::register('YFORM_DATA_LIST', function( $ep ) {   
-        $list = $ep->getSubject(); 
-        $list->setColumnFormat('preview_image', 'custom', function ($params ) {
-            return '<img style="width: 150px; height: auto;" src="'.$params['list']->getValue('preview_image').'">';
+if (rex::isBackend()) {
+    if (rex_request('table_name') == 'rex_collect_vimeo' or rex_be_controller::getCurrentPage() == 'collect/vimeo') {
+        rex_extension::register('YFORM_DATA_LIST', function ($ep) {
+            $list = $ep->getSubject();
+            $list->setColumnFormat('preview_image', 'custom', function ($params) {
+                return '<img style="width: 150px; height: auto;" src="' . $params['list']->getValue('preview_image') . '">';
+            });
         });
-    });
+    }
 }
-							
-							
-							
-
-
