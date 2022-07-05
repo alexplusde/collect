aa4c3615df4e46e0b99d20136418b0d7492c028f
Thomas Skerbis <skerbis@klxm.de>
2022-07-05 14:57:06 +0200

    Update boot.php

--- plugins/vimeo/boot.php
+++ plugins/vimeo/boot.php
@@ -1,18 +1,4 @@
 <?php
-
-if (rex_addon::get('yform')->isAvailable() && !rex::isSafeMode()) {
-    rex_yform_manager_dataset::setModelClass(
-        'rex_collect_vimeo',
-        collect_vimeo::class
-    );
-}
-if (rex_addon::get('cronjob')->isAvailable()) {
-    rex_cronjob_manager::registerType(rex_cronjob_collect_video_vimeo::class);
-}
-// Show poster images from Vimeo.com 
-
-<?php
-
 if (rex_addon::get('yform')->isAvailable() && !rex::isSafeMode()) {
     rex_yform_manager_dataset::setModelClass(
         'rex_collect_vimeo',
